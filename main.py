2feead4dabc2a2410ab882f32a2053c57bd8fa2c
blueliner21 <blueliner21@gmail.com>
2023-06-14 20:41:54 -0600

    Started code check and added display code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import machine
 import time
-#from ssd1306 import SSD1306_I2C
+from ssd1306 import SSD1306_SPI
 
+#Code 144965
 #6-13-23 To Do: Add code check algo. Print to OLED 
+
+SCK = machine.Pin(2)
+MOSI = machine.Pin(3)
+RES = machine.Pin(29)
+DC = machine.Pin(28)
+CS = machine.Pin(27)
 
 R1 = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
 R2 = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_DOWN)
@@ -13,12 +20,21 @@ C1 = machine.Pin(20, machine.Pin.OUT)
 C2 = machine.Pin(21, machine.Pin.OUT)
 C3 = machine.Pin(22, machine.Pin.OUT)
 
+OLED = machine.SPI(0, baudrate=400000, sck=SCK, mosi = MOSI)
+disp = SSD1306_SPI(width=128,height=64,spi=OLED,dc=DC,res=RES,cs=CS)
+
+disp.text("1",0,32)
+disp.text("2",10,32)
+disp.show()
+
+debounce_time = 200
+pinCode = [1,4,4,9,6,5]
 code = []
 code.clear()
 def enter_pressed():
     print(code)
-    for items in code:
-        print(code[items])
+    for i in code:
+        print(i)
     
     
 
@@ -27,53 +43,53 @@ while True:
     if R1.value():
         #print("7")
         code.append(7)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R2.value():
         #print("4")
         code.append(4)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R3.value():
         #print("1")
         code.append(1)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R4.value():
         print("CLEAR")
         code.clear()
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     C1.value(0)
     C2.value(1)
     if R1.value():
         #print("8")
         code.append(8)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R2.value():
         #print("5")
         code.append(5)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R3.value():
         #print("2")
         code.append(2)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R4.value():
         #print("0")
         code.append(0)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     C2.value(0)
     C3.value(1)
     if R1.value():
         #print("9")
         code.append(9)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R2.value():
         #print("6")
         code.append(6)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R3.value():
         #print("3")
         code.append(3)
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     if R4.value():
         #print("Enter")
         enter_pressed()
-        time.sleep_ms(250)
+        time.sleep_ms(debounce_time)
     C3.value(0)
